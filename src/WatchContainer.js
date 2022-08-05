ad6c71eb5f7ce11705c9b6cc59bdd626e038e73a
Braylan Dodson <braylan1005@gmail.com>
2022-08-05 11:38:34 -0400

    fixed watches not populating

--- src/WatchContainer.js
+++ src/WatchContainer.js
@@ -1,6 +1,6 @@
-import WatchCard from "react"
+import WatchCard from "./WatchCard.js"
 
-function WatchContainer(watches){
+function WatchContainer({watches}){
     const watchCardList = watches.map((watch)=>(
         <WatchCard key={watch.id} watch={watch}/>
     ))
